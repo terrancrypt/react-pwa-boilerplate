4ae83cd57db3a7c9d5f6c25b04cf543884448232
terrancypt <thaiphamngoctuong@gmail.com>
2025-05-15 20:40:13 +0700

    Add PWA support and update initial app layout with background color

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ function App() {
 
 
   return (
-    <>
-    </>
+    <div className="bg-amber-300 h-screen w-screen">
+      <h1>hello</h1>
+    </div>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({ registerType: 'autoUpdate' })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
